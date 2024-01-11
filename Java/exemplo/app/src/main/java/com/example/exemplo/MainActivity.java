6300c95a865b664a418649b1ba924bafece7d9cb
Luiz Eduardo <luiztecinfor@gmail.com>
2024-01-10 20:00:59 -0400

    Atualizacao do app
    Adicao da ListView para exibir a programacao na lista

--- Java/exemplo/app/src/main/java/com/example/exemplo/MainActivity.java
+++ Java/exemplo/app/src/main/java/com/example/exemplo/MainActivity.java
@@ -1,41 +1,43 @@
 package com.example.exemplo;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+    private ListView listView;
     private CalendarView calendarView;
     private RequestQueue requestQueue;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> dataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
         calendarView = findViewById(R.id.calendarView);
         requestQueue = Volley.newRequestQueue(this);
+        dataList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(adapter);
 
         // Registrar um listener para a data selecionada no CalendarView
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -43,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 Calendar selectedDate = Calendar.getInstance();
                 selectedDate.set(year, month, dayOfMonth);
+
+                // Limpar a lista existente antes de buscar os novos dados
+                dataList.clear();
+                adapter.notifyDataSetChanged();
+
                 getData(selectedDate);
             }
         });
@@ -70,9 +77,9 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject programObject = entryObject.getJSONObject("program");
 
                         // Agora, obtenha a URL da logo dentro do objeto custom_info -> Graficos -> LogoURL
-                        JSONObject customInfoObject = entryObject.getJSONObject("custom_info");
-                        JSONObject graphicsObject = customInfoObject.getJSONObject("Graficos");
-                        String logoURL = graphicsObject.getString("LogoURL");
+                        //JSONObject customInfoObject = entryObject.getJSONObject("custom_info");
+                        //JSONObject graphicsObject = customInfoObject.getJSONObject("Graficos");
+                        //String logoURL = graphicsObject.getString("LogoURL");
 
 
                         // Obter horários de início e término
@@ -83,14 +90,15 @@ public class MainActivity extends AppCompatActivity {
                         String programName = programObject.getString("name");
 
                         // Construir a string a ser exibida
-                        String entryText = logoURL + startTime + " - " + programName + "\n";
+                        String entryText = startTime + " - " + programName + "\n";
 
                         // Adicionar a entrada ao texto geral
-                        dataText.append(entryText);
+                        dataList.add(entryText);
 
                     }
 
-                    textView.setText(dataText.toString());
+                    // Notificar o adaptador sobre as mudanças nos dados
+                    adapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
